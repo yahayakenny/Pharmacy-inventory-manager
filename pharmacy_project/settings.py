8f12b4d5e0cd8a9f9eb422373e3b5dfb9d07004e
Yahaya Kehinde <mac@Yahaya-Kehinde-macbook.local>
2020-10-28 09:05:56 +0100

    added heroku postgresql database

--- pharmacy_project/settings.py
+++ pharmacy_project/settings.py
@@ -92,8 +92,12 @@ DATABASES = {
     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 }
 
-
 }
+
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
